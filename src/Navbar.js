b25b8689c2f5176a7f7984b7d43a8f069b48ece2
hari2u96 <hari2u96@gmail.com>
2023-02-02 11:01:41 +0530

    Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { Icon } from 'react-icons-kit'
+import {menu} from 'react-icons-kit/feather/menu'
+import {x} from 'react-icons-kit/feather/x'
+import "./Navbar.css" 
+
+const Navbar = () => {
+    const [toggle,setToggle]=useState(false)
+
+    const handleToggle =()=>{
+        setToggle(!toggle)
+    }
+  return (
+    <nav className={toggle? 'navbar expanded': 'navbar'}>
+     <h2 className='logo'>Qubernet</h2>
+     <ul className='links'>
+        <li>Home</li>
+        <li>Service</li>
+        <li>Data Science</li>
+        <li>Contact</li>
+     </ul>
+     <div className='toggle' onClick={handleToggle}>
+         {toggle?<Icon icon={x} size={28}/>:<Icon icon={menu} size={28}/>}
+     </div>
+    </nav>
+  )
+}
+
+export default Navbar
